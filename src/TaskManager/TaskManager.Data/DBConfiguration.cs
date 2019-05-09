aa98d91e2d4536b568b3a20d4ab0ea0d375bac69
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-05-09 18:06:00 +0530

    EF Setup

---
+++ src/TaskManager/TaskManager.Data/DBConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Data
+{
+    public sealed class DBConfiguration : DbMigrationsConfiguration<TaskDataContext>
+    {
+        public DBConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = false;
+        }
+
+        protected override void Seed(TaskDataContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
