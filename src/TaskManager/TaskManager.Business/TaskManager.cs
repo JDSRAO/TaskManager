023c7422ef1d8f086d1ff94ff57386abdeff8340
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-05-09 19:24:12 +0530

    Implemented get task

--- src/TaskManager/TaskManager.Business/TaskManager.cs
+++ src/TaskManager/TaskManager.Business/TaskManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using TaskManager.Data;
 using TaskManager.Models;
 
@@ -10,9 +11,19 @@ namespace TaskManager.Business
     {
         private TaskDataContext context = new TaskDataContext();
 
-        public List<UserTask> GetAllTasks()
+        public List<UserTask> GetTasks(Func<UserTask,bool> predicate)
         {
-            return context.UserTasks.ToList();
+            return context.UserTasks.Where(predicate).ToList();
+        }
+
+        public UserTask GetTask(Guid id)
+        {
+            var task = context.UserTasks.Where(x => x.ID == id).FirstOrDefault();
+            if(task != null)
+            {
+                return task;
+            }
+            throw new KeyNotFoundException("Task not found");
         }
     }
 }
