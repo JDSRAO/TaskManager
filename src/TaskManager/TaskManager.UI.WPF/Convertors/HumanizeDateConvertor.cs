4444f3d3e1df02f77dc80877d4b621aca4db6fd6
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-05-17 14:51:35 +0530

    Added Humanize package for humanizing date and time

---
+++ src/TaskManager/TaskManager.UI.WPF/Convertors/HumanizeDateConvertor.cs
@@ -0,0 +1,38 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TaskManager.Models.DTO;
+
+namespace TaskManager.UI.WPF.Convertors
+{
+    public class HumanizeDateConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is DateTime)
+            {
+                var date = (DateTime) value;
+                return date.Humanize();
+            }
+            else if(value is TimeSpan)
+            {
+                var date = (TimeSpan)value;
+                return date.Humanize();
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
