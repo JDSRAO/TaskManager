737f4a8bdf268f21c097da5515bc95e4e7cd65d1
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-05-20 14:07:34 +0530

    view model setup

--- src/TaskManager/TaskManager.UI.WPF/Views/TaskWindow/TaskWindowModel.cs
+++ src/TaskManager/TaskManager.UI.WPF/Views/TaskWindow/TaskWindowModel.cs
@@ -8,5 +8,9 @@ namespace TaskManager.UI.WPF.Views.TaskWindow
 {
     public class TaskWindowModel
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime TargetDate { get; set; }
     }
 }
