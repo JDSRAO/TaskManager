98a243d27be1ee9f40df06100d32ce891415cb7b
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-05-28 17:00:23 +0530

    Control setup

--- src/TaskManager/TaskManager.UI.WPF/Views/TaskListView/TaskListViewModel.cs
+++ src/TaskManager/TaskManager.UI.WPF/Views/TaskListView/TaskListViewModel.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TaskManager.Business;
 using TaskManager.Models.DTO;
+using TaskManager.UI.WPF.Commands;
 
 namespace TaskManager.UI.WPF.Views.TaskListView
 {
     public class TaskListViewModel
     {
         public List<UserTaskDto> Tasks { get; set; }
-
-        private TaskManagerService taskManager = new TaskManagerService();
+        public ICommand StartTaskCommand { get; set; }
+        public ICommand PauseTaskCommand { get; set; }
+        public ICommand EndTaskCommand { get; set; }
+        private TaskManagerService taskManager { get; }
 
         public TaskListViewModel()
         {
-            Tasks = taskManager.GetTasks().OrderByDescending(x => x.TargetDate).ToList();
+            taskManager = new TaskManagerService();
+            Tasks = taskManager.GetTasks().Where(x => x.IsEnded != true).OrderByDescending(x => x.TargetDate).ToList();
+            StartTaskCommand = new RelayCommand(ModifyTask);
+            PauseTaskCommand = new RelayCommand(ModifyTask);
+            EndTaskCommand = new RelayCommand(ModifyTask);
+        }
+
+        private void ModifyTask(object obj)
+        {
+            Task.Run(() => 
+            {
+                Console.WriteLine("Async task");
+            });
         }
     }
 }
