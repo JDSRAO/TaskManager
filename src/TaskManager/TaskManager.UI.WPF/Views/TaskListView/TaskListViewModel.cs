e2ef88769c5c1ff9d31e0b03b707d1bd862dab47
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2019-06-03 14:24:46 +0530

    Data binding fix

--- src/TaskManager/TaskManager.UI.WPF/Views/TaskListView/TaskListViewModel.cs
+++ src/TaskManager/TaskManager.UI.WPF/Views/TaskListView/TaskListViewModel.cs
@@ -11,25 +11,39 @@ using TaskManager.UI.WPF.Commands;
 
 namespace TaskManager.UI.WPF.Views.TaskListView
 {
-    public class TaskListViewModel
+    public class TaskListViewModel : BaseViewModel
     {
-        public ObservableCollection<UserTaskDto> Tasks { get; set; }
         public ICommand StartTaskCommand { get; set; }
         public ICommand PauseTaskCommand { get; set; }
         public ICommand EndTaskCommand { get; set; }
         public ICommand DeleteTaskCommand { get; set; }
         public ICommand SearchTasksCommand { get; set; }
 
+        public ObservableCollection<UserTaskDto> Tasks
+        {
+            get => tasks;
+            set
+            {
+                tasks = value;
+                OnPropertyChanged("Tasks");
+            }
+        }
+
         public string SearchQuery
         {
             get => searchQuery;
-            set { searchQuery = value; }
+            set
+            {
+                searchQuery = value;
+                OnPropertyChanged("SearchQuery");
+            }
         }
 
         public event EventHandler ActionExecuted;
 
         private TaskManagerService taskManager { get; }
         private string searchQuery { get; set; }
+        private ObservableCollection<UserTaskDto> tasks { get; set; }
 
         public TaskListViewModel()
         {
@@ -78,14 +92,20 @@ namespace TaskManager.UI.WPF.Views.TaskListView
         private void SearchTasks(object obj)
         {
             var query = searchQuery.ToLower();
-            var tasks = taskManager.GetTasks(x => x.Title.ToLower().Contains(query) || x.Description.ToLower().Contains(query)).OrderByDescending(x => x.TargetDate).ToList();
-            Tasks = new ObservableCollection<UserTaskDto>(tasks);
+            var filteredTasks = taskManager.GetTasks(x => x.Title.ToLower().Contains(query) || x.Description.ToLower().Contains(query)).OrderByDescending(x => x.TargetDate).ToList();
+            SetTasks(filteredTasks);
         }
 
         private void GetTasks(bool isEnded = false)
         {
-            var tasks = taskManager.GetTasks(x => x.IsEnded == isEnded).OrderByDescending(x => x.TargetDate).ToList();
-            Tasks = new ObservableCollection<UserTaskDto>(tasks);
+            var dbTasks = taskManager.GetTasks(x => x.IsEnded == isEnded).OrderByDescending(x => x.TargetDate).ToList();
+            SetTasks(dbTasks);
+        }
+
+        private void SetTasks(List<UserTaskDto> filteredTasks)
+        {
+            tasks = new ObservableCollection<UserTaskDto>(filteredTasks);
+            OnPropertyChanged("Tasks");
         }
 
         private void Notify()
