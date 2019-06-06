fcd223b0254d9377ec73efab70f763ae73bce7c5
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-06-06 16:58:26 +0530

    Dotnet framework udpate

--- src/TaskManager/TaskManager.NotificationHub/NotificationHub.cs
+++ src/TaskManager/TaskManager.NotificationHub/NotificationHub.cs
@@ -5,15 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Business;
 
 namespace TaskManager.NotificationHub
 {
     [HubName("TaskManagerHub")]
     public class NotificationHub : Hub
     {
+        private NotificationManager notificationManagerManager = new NotificationManager();
+
         public void PushNotifications()
         {
-            Clients.All.addMessage("");
+            var startDate = Convert.ToDateTime("2019-05-31 17:00:00.433");
+            var notifications = notificationManagerManager.GetNotifications(startDate);
+            if (notifications.Any())
+            {
+                Clients.All.addMessage(notifications);
+            }
         }
     }
 }
