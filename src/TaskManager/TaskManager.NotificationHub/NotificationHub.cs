03d2c1aac621f43005db9638a47de0af5f2ecb08
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-06-06 16:49:30 +0530

    Notification Hub Setup

---
+++ src/TaskManager/TaskManager.NotificationHub/NotificationHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.NotificationHub
+{
+    [HubName("TaskManagerHub")]
+    public class NotificationHub : Hub
+    {
+        public void PushNotifications()
+        {
+            Clients.All.addMessage("");
+        }
+    }
+}
