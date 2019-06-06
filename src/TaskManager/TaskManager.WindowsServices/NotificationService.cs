5f4840d1cd76d52d69a3f12d817a51509db9e59f
KJDS Srinivasa Rao <kjdssrinivasarao@gmail.com>
2019-06-06 17:16:35 +0530

    Business logic integration

--- src/TaskManager/TaskManager.WindowsServices/NotificationService.cs
+++ src/TaskManager/TaskManager.WindowsServices/NotificationService.cs
@@ -7,13 +7,24 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Business;
+using System.Timers;
+using TaskManager.Models.DTO;
 
 namespace TaskManager.WindowsServices
 {
     public partial class NotificationService : ServiceBase
     {
+        private static NotificationManager notificationManagerManager = new NotificationManager();
+        private Timer timer { get; }
+
+
         public NotificationService()
         {
+            timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(PushNotificationsToClient);
+            timer.Interval = 5000;
+            timer.Enabled = true;
             InitializeComponent();
         }
 
@@ -23,6 +34,17 @@ namespace TaskManager.WindowsServices
 
         protected override void OnStop()
         {
+        }
+
+        private void PushNotificationsToClient(object sender, ElapsedEventArgs e)
+        {
+            var startDate = Convert.ToDateTime("2019-05-31 17:00:00.433");
+            var notifications = new List<UserTaskDto>();
+            notifications = notificationManagerManager.GetNotifications(startDate);
+            if (notifications.Any())
+            {
+                //call hub
+            }
         }
     }
 }
